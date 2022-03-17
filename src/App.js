2b25709a1682def590f2cd618704466f0452b33b
Admin <admin.push@hackerrank.workspace>
2022-03-17 14:47:19 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import 'h8k-components';
 
@@ -6,38 +6,38 @@ import Articles from './components/Articles';
 
 const title = "Sorting Articles";
 
-function App({articles}) {
+function App({ articles }) {
 
-    const [sorted,setSorted] = React.useState([]);
+  const [sorted, setSorted] = React.useState([]);
 
-    useEffect(() => {
-      sortByVoted();
-      // eslint-disable-next-line
-    },[]);
-   
-    const sortByRecent = () => {
-      const theArticles = [...articles];
-      theArticles.sort((a,b) => new Date(b.date) -  new Date(a.date));
-      setSorted(theArticles);
-    }
+  useEffect(() => {
+    sortByVoted();
+    // eslint-disable-next-line
+  }, []);
 
-    const sortByVoted = () => {
-      const theArticles = [...articles];
-      theArticles.sort((a,b) => b.upvoted - a.upvoted);
-      setSorted(theArticles);
-    }
-    
-    return (
-        <div className="App">
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="layout-row align-items-center justify-content-center my-20 navigation">
-                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small" onClick={sortByVoted}>Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small" onClick={sortByRecent}>Most Recent</button>
-            </div>
-            <Articles articles={sorted}/>
-        </div>
-    );
+  const sortByRecent = () => {
+    const theArticles = [...articles];
+    theArticles.sort((a, b) => new Date(b.date) - new Date(a.date));
+    setSorted(theArticles);
+  }
+
+  const sortByVoted = () => {
+    const theArticles = [...articles];
+    theArticles.sort((a, b) => b.upvoted - a.upvoted);
+    setSorted(theArticles);
+  }
+
+  return (
+    <div className="App">
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className="layout-row align-items-center justify-content-center my-20 navigation">
+        <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
+        <button data-testid="most-upvoted-link" className="small" onClick={sortByVoted}>Most Upvoted</button>
+        <button data-testid="most-recent-link" className="small" onClick={sortByRecent}>Most Recent</button>
+      </div>
+      <Articles articles={sorted} />
+    </div>
+  );
 
 }
 
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Articles({articles}) {
+function Articles({ articles }) {
 
   return (
     <div className="card w-50 mx-auto">
